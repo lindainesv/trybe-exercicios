f8b0e6b9fb6dc6f0450ec89fc2e092782ab54a4b
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-21 15:06:47 -0300

    para fixar 1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para_fixar.js
@@ -0,0 +1,9 @@
+const myName = "Linda Ines Vicente";
+const birthCity = "Petrópolis";
+// birthCity = "onibus";
+let birthYear = 1995;
+// birthYear = 2030;
+
+console.log(birthCity);
+console.log(birthYear);
+console.log(myName);
