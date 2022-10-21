df10a65f43837d8d4fa8e5a6214f486cf9fa74ca
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-21 18:46:36 -0300

    alterei o sinal de + para *

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para_fixar_2.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para_fixar_2.js
@@ -1,6 +1,6 @@
 const base = 5;
 const heigth = 8;
-const area = base + heigth;
+const area = base * heigth;
 const perimeter = base + heigth + area;
 
 console.log("o perimetro do triagulo é", perimeter);
