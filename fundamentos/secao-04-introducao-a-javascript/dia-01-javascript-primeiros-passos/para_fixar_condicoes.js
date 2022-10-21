e19e57130cfcb2925125faf106d61660be2a3a74
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-21 20:29:58 -0300

    condições

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para_fixar_condicoes.js
@@ -0,0 +1,9 @@
+const candidato1 = 25;
+
+if (candidato1 >= 80) {
+  console.log("Parabens, você foi aprovada(o)!");
+} else if (candidato1 < 80 && candidato1 >=60) {
+  console.log("Voce está na nossa lista de espera");
+} else if (candidato1 < 60) {
+  console.log ("Voce foi reprovado(o)");
+}
