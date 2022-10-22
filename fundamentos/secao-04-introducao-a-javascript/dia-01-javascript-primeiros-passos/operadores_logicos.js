3919406cd0ef5681449c3b00bde915ccba271218
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-21 22:56:18 -0300

    correção

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/operadores_logicos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/operadores_logicos.js
@@ -1,16 +1,16 @@
-const currentHour = 10;
+const currentHour = 23;
 let message = "";
 
 if (currentHour >= 22) {
-	console.log("Não deveriamos comer nada, é hora de dormir.");
+	message="Não deveriamos comer nada, é hora de dormir." ;
 } else if (currentHour >= 18 && currentHour < 22) {
-	console.log("Rango da noite, vamos jantar :D");
+	message="Rango da noite, vamos jantar :D" ;
 } else if (currentHour >= 14 && currentHour<18) {
-	console.log("Vamos fazer um bolo pro café da tarde?");
+	message="Vamos fazer um bolo pro café da tarde?" ;
 } else if (currentHour >= 11 && currentHour <14) {
-	console.log("Hora do almoço!!!");
+	message="Hora do almoço!!!" ;
 } else if (currentHour >= 4 && currentHour <11) {
-	console.log("Hmmm, cheirinho de café recém-passado");
+	message = "Hmmm, cheirinho de café recém-passado";
 } 
 
 console.log(message);
