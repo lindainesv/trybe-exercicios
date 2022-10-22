85d646fc0dfbea24597dabb4a8e5e17f9e94d497
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-22 00:04:49 -0300

    ainda nao resolvido o or

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/para_fixar_or****.js
@@ -0,0 +1,14 @@
+const weekDay = "segunda-feira";
+let aviso =  "";
+
+if (weekDay == "segunda-feira" || 
+weekDay == "terça-feira"||
+ weekDay == "quarta-feira"||
+ weekDay =="quinta-feira" || 
+ weekDay == "sexta-feira") {
+	console.log ("Oba, mais um dia de aprendizado na Trybe >:D");
+} else (weekDay == "sabado" || 
+weekDay == "domingo"); {
+	console.log ("FINALMENTE, descanso merecido UwU");
+}
+
