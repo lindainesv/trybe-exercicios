c0a8a0e8dfa813aace6acf1018810188a0c533e1
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-24 22:45:28 -0300

    exercicio 3

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio_1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio_1.js
@@ -1,15 +1,37 @@
-const n1 = 5;
+const n1 = 1;
 const n2 = 3;
 const n3 = 14;
 
-console.log(n1+n2);
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. 
+
+  console.log(n1+n2);
 	console.log(n1-n2);
-	console.log (n1*n2);
-	console.log (n1/n2);
-	console.log (n2%n2);
+	console.log(n1*n2);
+	console.log(n1/n2);
+	console.log(n2%n2);
 
+
+//Utilize if/else para fazer um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
 	if (n1 > n2) {
-	 console.log ();
+	 console.log (n1+ " é maior que "+ n2 + ".");
+	} else {
+		console.log (n2 +" é maior que "+ n1 + ".");
+	};
 
-	}
+//Utilize if/else para fazer um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+
+	if (n1 >n2 && n1 >n3){
+		console.log ("O maior numero é "+ n1 + "(a)");
+	} else if  (n2 >n1 && n2 >n3) {
+		console.log ("O maior numero é "+ n2 + " (b)");
+}  else if (n3 >n2 && n3 >n1) {
+		console.log ("O maior numero é "+ n3 + " (c)");
+} 
+
+
+
+
+
+
+
 
