c58dc26aaa9f4511f39387d75c512f497803eeef
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-25 20:18:11 -0300

    inclusao de exercio de 'lucro'

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio_ha_um_par_entre_nos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio_ha_um_par_entre_nos.js
@@ -1,14 +1,24 @@
 //Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
 //Bonus: use somente um if.
 
-const n1 = 3;
-const n2 = 9;
-const n3 = 13;
+const n1 = 2;
+const n2 = 4;
+const n3 = 6;
+
+let ePar = false;
+if (n1 % 2 === 0 || n2 % 2 === 0|| n3 % 2 === 0) {
+	ePar = true
+}; 
+	console.log(ePar);
+
+
+//Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+//Bonus: use somente um if.
+
 
 let eImpar = false;
+if (n1 % 2 !== 0 || n2 % 2 !== 0 || n3 % 2 !== 0) {
+	eImpar = true
+};
+	console.log(eImpar);
 
-if (n1 % 2 === 0 || n2 % 2 === 0|| n3 % 2 === 0) {
-	eImpar = true;
-}; 
-
-console.log(eImpar);
