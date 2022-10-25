2473ffdf31dbdb41cfaa8f8f920f730fafec55bd
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-24 22:06:05 -0300

    primeiro exercicio

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio_1.js
@@ -0,0 +1,15 @@
+const n1 = 5;
+const n2 = 3;
+const n3 = 14;
+
+console.log(n1+n2);
+	console.log(n1-n2);
+	console.log (n1*n2);
+	console.log (n1/n2);
+	console.log (n2%n2);
+
+	if (n1 > n2) {
+	 console.log ();
+
+	}
+
