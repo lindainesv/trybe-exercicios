746aca72a18f01324e19243ebb3b167bf67edea9
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-26 22:05:54 -0300

    resolução exercicio arrow functions

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-05/exercicios_part_1_arrow_functions.js
@@ -0,0 +1,10 @@
+//🚀 Transforme a função numeroAleatorio em uma arrow function;
+
+// function numeroAleatorio() {
+// 	return Math.random()
+// }
+// console.log(numeroAleatorio());
+
+const numeroAleatorio = () => Math.random()
+console.log(numeroAleatorio());
+
