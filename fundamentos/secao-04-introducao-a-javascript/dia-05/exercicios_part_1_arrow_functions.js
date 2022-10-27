d060afdd286fbf18215ec2a0fd9e7d0d80566294
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-26 22:36:31 -0300

    resolução exercio nome arrow

--- fundamentos/secao-04-introducao-a-javascript/dia-05/exercicios_part_1_arrow_functions.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-05/exercicios_part_1_arrow_functions.js
@@ -5,6 +5,17 @@
 // }
 // console.log(numeroAleatorio());
 
-const numeroAleatorio = () => Math.random()
-console.log(numeroAleatorio());
+// const numeroAleatorio = () => Math.random()
+// console.log(numeroAleatorio());
 
+// Transforme a função hello em uma arrow function;
+// function hello(nome) {
+// 	return `Olá, ${nome}!`
+// }
+// let nome = 'Ivan';
+// console.log(hello(nome));
+
+
+const hello = nome => `Ola, ${nome}!`
+let nome  = `Linda `
+console.log(hello(nome));
