f7b4af4c5865e010c4b0c5c2f25263a4001fa072
lindainesv <lindainesvicente@hotmail.com.com>
2022-11-02 18:12:07 -0300

    resolução atividade

--- fundamentos/secao-05-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/removendo-elementos-script.js
+++ fundamentos/secao-05-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/removendo-elementos-script.js
@@ -1,26 +1,27 @@
 // Remova todos os elementos filhos de paiDoPai exceto pai, elementoOndeVoceEsta e primeiroFilhoDoFilho.
+//  1. Recuperar o elemento com o id pai
+const acessoPai = document.getElementById("pai");
 
-/* const mandandoEmBoraOsFilhos = () => {
-let acessoAoPai = document.getElementById("pai");
-let filhosDePaiDoPai = pai.childNodes;
-for (let i = i; filhosDePaiDoPai.lengt - 1; i>=0; i-=1){
-	const
+//  2. Utilize o .childNodes para retornar uma coleção viva de nós filhos do elemento pai.
+const todosFilhos = acessoPai.childNodes;
+
+//  3. Terceiro passo: Utilize uma estrutura de repetição para iterar sob o array todosOsFilhos.
+//  Utilize o .length para verificar o tamanho que todosOsFilhos possui;
+//  Como a primeira posição de um array é a posição 0, subtraia 1 do tamanho de todosOsFilhos;
+//  Decremente o index a cada iteração.
+for (let i = todosFilhos.length -1; i >= 0; i-=1) {
+
+//  4. Armazene o filho atual em uma variável.
+	const filhoAtual = todosFilhos[i]; 
+
+// 5. Caso o id do filhoAtual seja diferente de elementoOndeVoceEsta, remova o filhoAtual.
+		if (filhoAtual.id !== 'elementoOndeVoceEsta') {
+		filhoAtual.remove();
+		}
 }
 
-console.log(filhosDePaiDoPai);
-
-}; mandandoEmBoraOsFilhos (); */
-
-
+// 6. Recupere o elemento com o id segundoEUltimoFilhoDoFilho e o remova.
+const segundoEUltimoFilhoDoFilho = document.getElementById("segundoEUltimoFilhoDoFilho");
+segundoEUltimoFilhoDoFilho.remove();
 
 
-    const pai = document.getElementById('pai');
-    const todosOsFilhos = pai.childNodes;
-    for (let index = todosOsFilhos.length - 1; index >= 0; index -= 1) {
-      const filhoAtual = todosOsFilhos[index];
-      if (filhoAtual.id !== 'elementoOndeVoceEsta') {
-        filhoAtual.remove();
-      }
-    }
-    const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
-    segundoEUltimoFilhoDoFilho.remove();
