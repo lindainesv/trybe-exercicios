6608fdf4d934d77bbac3453235e12f31bb11988f
lindainesv <lindainesvicente@hotmail.com.com>
2022-12-07 21:10:30 -0300

    excluindo espaços

--- fundamentos/secao-08-higher-order-functions-do-javaScript-ES6/dia-02-higher-order-functions-sort-e-map/sort-exemplo.js
+++ fundamentos/secao-08-higher-order-functions-do-javaScript-ES6/dia-02-higher-order-functions-sort-e-map/sort-exemplo.js
@@ -2,12 +2,6 @@ const food = ['arroz', 'feijão', 'farofa', 'chocolate', 'doce de leite', 'abaca
 food.sort();
 console.log(food);
 
-
-
-
-
-
-
 const inventory = [1, 3, 2, 5, 4, 6, 7, 8, 9, 10];
 // troca a operação para b - a
 inventory.sort((a, b) => b - a);
