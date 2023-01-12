e059b1b5f4448d3b9eb1e4f776a37a05673fe380
Linda Ines Vicente <lindainesvicente@hotmail.com>
2023-01-12 13:04:07 -0300

    resolução exercicio microblogs

--- front-end/secao-01-introducao-ao-front-end-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/exercicio-de-fixacao/microblogs/src/main.js
+++ front-end/secao-01-introducao-ao-front-end-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fetch/exercicio-de-fixacao/microblogs/src/main.js
@@ -1,4 +1,4 @@
-import '../style.css';
+import './style.css';
 
 import {
   fillUsersSelect,
@@ -13,24 +13,37 @@ const usersSelect = document.querySelector('#users-select');
 const USERS_API = 'https://dummyjson.com/users';
 // lógica para pegar as informações das pessoas usuárias e preencher o select aqui.
 fetch(USERS_API)
-  .then((resposta) => resposta.json())
+  .then((response) => response.json())
   .then((data) => {
     const { users } = data;
     
     fillUsersSelect(users);
 });
 
-usersSelect.addEventListener('change', () => {
+usersSelect.addEventListener('change', (event) => {
   clearPageData();
 
-  // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
-  const POSTS_API = `https://dummyjson.com/users/${usersSelect.value}`;
-  
+  const POSTS_API = `https://dummyjson.com/posts/user/${event.target.value}`;
+  // const POSTS_API = `https://dummyjson.com/posts/user/${usersSelect.value}`
+
   fetch(POSTS_API)
-    .then((resposta) => resposta.json())
+    .then((response) => response.json())
     .then((data) => {
       const { posts } = data;
-
       fillPosts(posts);
-    })  
+
+      const [featuredPost] = posts;
+      const COMMENTS_API =      
+       `https://dummyjson.com/posts/${featuredPost.id}/comments`;
+       return fetch(COMMENTS_API);
+    })
+    .then((res)=> res.json())
+    .then((data)=> {
+      const { comments } = data;
+      fillFeaturedPostComments(comments);
+    })
+    .catch((error) => {
+      fillErrorMessage('Erro ao recuperar as informações');
+      console.log(error.message);
+    });
 });
