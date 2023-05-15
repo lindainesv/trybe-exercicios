fbde5727997f4a117b5795312856a8062af99efa
lindainesv <lindainesvicente@hotmail.com.com>
2023-05-15 11:26:57 -0300

    exercicios dia 4.4

--- back-end/secao-04-/dia-04-/soccer-team-manager/src/app.js
+++ back-end/secao-04-/dia-04-/soccer-team-manager/src/app.js
@@ -1,31 +1,69 @@
+// src/app.js
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+let nextId = 3;
+
 const express = require('express');
 
 const app = express();
 
+// const apiCredentials = require('./middlewares/apiCredentials');
+// app.use(apiCredentials); 
+
+const validateTeam = require('./middlewares/validateTeam');
+
+const existingId = require('./middlewares/existingId');
+
+app.use(validateTeam);
+
 app.use(express.json());
 
-const teams = [
-  {
-    id: 1,
-    name: 'São Paulo Futebol Clube',
-    initials: 'SPF',
-  },
-  {
-    id: 2,
-    name: 'Clube Atlético Mineiro',
-    initials: 'CAM',
-  },
-];
+app.get('/teams', (req, res) => res.json(teams));
 
-// app.get('/teams', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+// app.get('/teams/:id', (req, res) => {
+//   const id = Number(req.params.id);
+//   const team = teams.find((t) => t.id === id);
+//   if (team) {
+//     res.json(team);
+//   } else {
+//     res.sendStatus(404);
+//   }
+// });
 
-app.get('/teams', (req, res) => res.status(200).json({ teams }));
+app.get('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  res.json(team);
+});
 
-app.post('/teams', (req, res) => {
-  const newTeam = { ...req.body };
-  teams.push(newTeam);
+// Arranja os middlewares para chamar validateTeam primeiro
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+});
 
-  res.status(201).json({ team: newTeam });
+app.put('/teams/:id', existingId, validateTeam, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+
+    // não precisamos mais conferir, com certeza o team existe, antes tinha um if e else
+    const index = teams.indexOf(team);
+    const updated = { id, ...req.body };
+    teams.splice(index, 1, updated);
+    res.status(201).json(updated);
+});
+
+app.delete('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
+  res.sendStatus(204);
 });
 
 module.exports = app;
