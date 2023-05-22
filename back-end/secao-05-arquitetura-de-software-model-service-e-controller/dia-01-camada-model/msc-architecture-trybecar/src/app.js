29ae7cff51da2eb8145548428c8ff260c6291185
lindainesv <lindainesvicente@hotmail.com.com>
2023-05-21 21:09:19 -0300

    refatorando app

--- back-end/secao-05-arquitetura-de-software-model-service-e-controller/dia-01-camada-model/msc-architecture-trybecar/src/app.js
+++ back-end/secao-05-arquitetura-de-software-model-service-e-controller/dia-01-camada-model/msc-architecture-trybecar/src/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-// const camelize = require('camelize'); // para entender melhor esse pacote vocÊ pode dar uma olhadinha aqui: https://www.npmjs.com/package/camelize
-const connection = require('./models/connection');
-const { travelModel } = require('./models');
+const { passengerModel, travelModel, waypointModel } = require('./models');
 
 const app = express();
 
@@ -13,21 +11,22 @@ const TRAVEL_IN_PROGRESS = 3;
 const TRAVEL_FINISHED = 4;
 
 const passengerExists = async (passengerId) => {
-  const [[passenger]] = await connection.execute(
-    'SELECT * FROM passengers WHERE id = ?',
-    [passengerId],
-  );
+  const passenger = await passengerModel.findById(passengerId);
   if (passenger) return true;
   return false;
 };
 
 const saveWaypoints = (waypoints, travelId) => {
   if (waypoints && waypoints.length > 0) {
-    return waypoints.map(async (value) => connection.execute(
-      'INSERT INTO waypoints (address, stop_order, travel_id) VALUE (?, ?, ?)',
-      [value.address, value.stopOrder, travelId],
-    ));
+    return waypoints.map(async (value) => {
+      await waypointModel.insert({
+        address: value.address,
+        stopOrder: value.stopOrder,
+        travelId,
+      });
+    });
   }
+
   return [];
 };
 
@@ -36,28 +35,15 @@ app.post('/passengers/:passengerId/request/travel', async (req, res) => {
   const { startingAddress, endingAddress, waypoints } = req.body;
 
   if (await passengerExists(passengerId)) {
-    // const [resultTravel] = await connection.execute(
-    //   `INSERT INTO travels 
-    //       (passenger_id, starting_address, ending_address) VALUE (?, ?, ?)`,
-    //   [
-    //     passengerId,
-    //     startingAddress,
-    //     endingAddress,
-    //   ],
-    // );
-
+    // Aqui substituímos o trecho de código SQL pela chamada a função insert do model
+    // e armazenamos o retorno da função na variável travelId 
     const travelId = await travelModel.insert({ passengerId, startingAddress, endingAddress });
 
+    // Renomeamos o parâmetro result.insertId para travelId
     await Promise.all(saveWaypoints(waypoints, travelId));
 
-    // const [[response]] = await connection.execute(
-    //   'SELECT * FROM travels WHERE id = ?',
-    //   [travelId],
-    // );
-
+    // Aqui substituímos a consulta SQL pela nossa função findById
     const travel = await travelModel.findById(travelId);
-
-    // return res.status(201).json(camelize(response));
     return res.status(201).json(travel);
   }
   res.status(500).json({ message: 'Ocorreu um erro' });
@@ -70,44 +56,24 @@ app.get('/drivers/open/travels', async (_req, res) => {
 
 app.put('/drivers/:driverId/travels/:travelId/assign', async (req, res) => {
   const { travelId, driverId } = req.params;
-  await connection.execute(
-    'UPDATE travels SET driver_id = ? WHERE id = ?',
-    [driverId, travelId],
-  );
-  await connection.execute(
-    'UPDATE travels SET travel_status_id = ? WHERE id = ? AND driver_id = ?',
-    [DRIVER_ON_THE_WAY, travelId, driverId],
-  );
-  const [[result]] = await connection.execute(
-    'SELECT * FROM travels WHERE id = ?',
-    [travelId],
-  );
-  res.status(200).json(result);
+
+  await travelModel.updateById(travelId, { driverId, travelStatusId: DRIVER_ON_THE_WAY });
+  const travel = await travelModel.findById(travelId);
+
+  res.status(200).json(travel);
 });
 
 app.put('/drivers/:driverId/travels/:travelId/start', async (req, res) => {
   const { travelId, driverId } = req.params;
-  await connection.execute(
-    'UPDATE travels SET travel_status_id = ? WHERE id = ? AND driver_id = ?',
-    [TRAVEL_IN_PROGRESS, travelId, driverId],
-  );
-  const [[result]] = await connection.execute(
-    'SELECT * FROM travels WHERE id = ?',
-    [travelId],
-  );
+  await travelModel.updateById(travelId, { driverId, travelStatusId: TRAVEL_IN_PROGRESS });
+  const result = await travelModel.findById(travelId);
   res.status(200).json(result);
 });
 
 app.put('/drivers/:driverId/travels/:travelId/end', async (req, res) => {
   const { travelId, driverId } = req.params;
-  await connection.execute(
-    'UPDATE travels SET travel_status_id = ? WHERE id = ? AND driver_id = ?',
-    [TRAVEL_FINISHED, travelId, driverId],
-  );
-  const [[result]] = await connection.execute(
-    'SELECT * FROM travels WHERE id = ?',
-    [travelId],
-  );
+  await travelModel.updateById(travelId, { driverId, travelStatusId: TRAVEL_FINISHED });
+  const result = await travelModel.findById(travelId);
   res.status(200).json(result);
 });
 
