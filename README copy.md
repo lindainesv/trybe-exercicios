b8b03efe5eefc0b509b65a0eb8b907b7e00b026e
lindainesv <lindainesvicente@hotmail.com.com>
2023-06-10 23:19:55 -0300

    commit main

--- README copy.md
+++
@@ -1,2 +0,0 @@
-# Meu repositório de exercícios	
-rascunho
--- fundamentos/secao-08-higher-order-functions-do-javaScript-ES6/dia-03-higher-order-functions-filter-e-reduce/reduce-somando-pares.js
+++
@@ -1,8 +0,0 @@
-// Faça uma função que some todos os números pares do array numeros:
-const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
-
-// Solução usando apenas reduce:
-const sum = (accumulator, number) =>
-  number % 2 === 0 ? accumulator + number : accumulator;
-const sumNumbers = (array) => array.reduce(sumPair, 0);
-console.log(sumNumbers(numbers)); // 152
