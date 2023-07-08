b98580208dafe6d97606a7f540776d72b1ab3f51
lindainesv <lindainesvicente@hotmail.com.com>
2023-07-08 16:47:57 -0300

    Para fixar parte 02

--- back-end/secao-09-programacao-orientada-a-objetos-(POO)-e-SOLID/dia-02-heranca-e-composicao/para-fixar/Superclass.ts
+++ back-end/secao-09-programacao-orientada-a-objetos-(POO)-e-SOLID/dia-02-heranca-e-composicao/para-fixar/Superclass.ts
@@ -9,9 +9,11 @@ class Superclass {
   //   const msg = 'Olá mundo!'
   //   console.log(msg);
   //   return this.sayHello;
-  // }
+  // } icorreto
 
-  public sayHello(): void { // metodo 'sayHello'
+  // protected 
+  // private
+   public sayHello(): void { // criação do metodo 'sayHello'
     const msg = 'Olá mundo!'
     console.log(msg);
   }
@@ -23,15 +25,29 @@ class Subclass extends Superclass { // outra classe criada
   //   const act = 'Gira'
   //   console.log(act);
   //   return this.sayHello;
-  // }
+  // } incorreto
+
+  constructor() {
+    super();
+    this.isSuper = false // setar
+  };
+
+  public sayHello2(): void { // criação do metodo 'sayHello2'
+    this.sayHello(); // chamando o metodo 'sayHello' da classe mãe 'Superclass'
+  }
 }
 
 const myFunc = (objSuper: Superclass) => { // criação da função 'myFunc' e chamada do metodo como parametro
-  objSuper.sayHello; // chamada do metodo 'sayHello' da Superclass
+  objSuper.sayHello(); // chamada do metodo 'sayHello' da Superclass
+  console.log(objSuper.isSuper ? 'Super!' : 'Sub!');
 };
 
-const sup = new Superclass; // criação de um objeto da Superclass
-const sub = new Subclass; // criação de um objeto da Subclass
+// const myFunc = (objSub: Subclass) => { // criação da função 'myFunc' e chamada do metodo como parametro
+//   objSub.sayHello2() // chamada do metodo 'sayHello2' da Subclass
+// };
+
+const sup = new Superclass(); // criação de um objeto da Superclass
+const sub = new Subclass(); // criação de um objeto da Subclass
 
 myFunc(sup); // chamando a função com o objeto criado
 myFunc(sub); // chamando a função com o objeto criado
