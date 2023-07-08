ea1b7bf4d70e0594b3877f994f67abe338939a59
lindainesv <lindainesvicente@hotmail.com.com>
2023-07-08 17:08:27 -0300

    Para Fixar 02 Gabarito

--- back-end/secao-09-programacao-orientada-a-objetos-(POO)-e-SOLID/dia-02-heranca-e-composicao/para-fixar/Superclass.ts
+++ back-end/secao-09-programacao-orientada-a-objetos-(POO)-e-SOLID/dia-02-heranca-e-composicao/para-fixar/Superclass.ts
@@ -5,20 +5,28 @@ class Superclass {
     this.isSuper = true // setar
   };
 
-  public sayHello(): void { // metodo 'sayHello'
+  protected sayHello(): void { // metodo 'sayHello'
     const msg = 'Olá mundo!'
     console.log(msg);
   }
 }
 
-class Subclass extends Superclass { } // outra classe criada
+class Subclass extends Superclass { // outra classe criada
+  public sayHello2(): void {
+    this.sayHello();
+  }
+} 
 
-const myFunc = (objSuper: Superclass) => { // criação da função 'myFunc' e chamada do metodo como parametro
-  objSuper.sayHello(); // chamada do metodo 'sayHello' da Superclass
+const myFunc = (objSuper: Subclass) => { // criação da função 'myFunc' e chamada do metodo como parametro
+  objSuper.sayHello2(); // chamada do metodo 'sayHello' da Superclass
 };
 
-const sup = new Superclass(); // criação de um objeto da Superclass
+// const sup = new Superclass(); // criação de um objeto da Superclass
 const sub = new Subclass(); // criação de um objeto da Subclass
 
-myFunc(sup); // chamando a função com o objeto criado
+// myFunc(sup); // chamando a função com o objeto criado
 myFunc(sub); // chamando a função com o objeto criado
+
+
+// Mude a visibilidade do método `sayHello` de _protected_ para _private_. O que acontece?
+// Ao mudar a visibilidade de _protected_ para _private_, o método `sayHello2` da _Subclass_ deixa de poder acessar o método `sayHello` definido na _Superclass_. Isso acontece pois um método privado só pode ser acessado na classe que o define.
