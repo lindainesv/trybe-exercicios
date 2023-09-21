599d7c69c4f9a966091e47d98b20fecd3c2bc4ae
lindainesv <lindainesvicente@hotmail.com>
2023-09-21 16:53:57 -0300

    inserindo arquivo

---
+++ python-eletiva/secao-01/dia-02/main.py
@@ -0,0 +1,14 @@
+def mean(numbers):
+    """
+    Calcula a média de uma lista de números.
+
+    >>> my_list = [1, 2, 3, 4, 5]
+    >>> mean(my_list)
+    3.0
+    >>> mean([2.5, 3.75, 1.25, 4])
+    2.875
+    >>> mean([])
+    0
+
+    """
+    return sum(numbers) / len(numbers)
